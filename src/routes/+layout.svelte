77485528c3a55c7aa9da5ef75a7b10eca1d44d35
Anastasia <anastasia.trombly@gmail.com>
2026-02-23 22:55:28 -0500

    change order again, idk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,10 +11,10 @@
 
 <div class="navbar bg-accent">
 	<div class="mx-auto flex w-full max-w-3xl items-center justify-between px-4">
+		<a href="https://www.akorl.xyz"><img src="/logo3.svg" alt="akorl.xyz" class="h-16" /></a>
 		<nav class="flex gap-4">
 			<a href="/" class="text-2xl link link-hover">Archive</a>
 		</nav>
-		<a href="https://www.akorl.xyz"><img src="/logo3.svg" alt="akorl.xyz" class="h-16" /></a>
 
 	</div>
 </div>
